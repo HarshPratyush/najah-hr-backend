2c2631997e8a85c8c4acc444618063323e05fb65
Harsh Pratyush <harshpratyush1206@gmail.com>
2020-03-30 04:44:33 +0530

    GCP added

--- src/main/java/in/co/najah/najahhr/entity/audit/AuditorAwareImpl.java
+++ src/main/java/in/co/najah/najahhr/entity/audit/AuditorAwareImpl.java
@@ -1,6 +1,5 @@
 package in.co.najah.najahhr.entity.audit;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.data.domain.AuditorAware;
 
 import java.util.Optional;
@@ -9,14 +8,6 @@ public class AuditorAwareImpl  implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//
-//        if (authentication == null || !authentication.isAuthenticated()) {
-//            return "anynomus";
-//        }
-//
-//        return ((UserDetails) authentication.getPrincipal()).getUser();
-
         return Optional.of("anonymous");
     }
 }
